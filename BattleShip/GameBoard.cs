9755eba8efd6b8d65a0d8803792ba12852616016
matt <matt.breck@live.com>
2019-04-04 15:59:41 -0400

    new stuff

---
+++ BattleShip/GameBoard.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    class GameBoard
+    {
+    }
+    public class Cordinates
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Cordinates(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+
+    public class Occupation
+    {
+        private Dictionary<string, string> occupationType = new Dictionary<string, string>()
+        {
+            {"Empty", "O" },
+            {"Battleship","B" },
+            {"Cruiser","C" },
+            {"Destroyer","D" },
+            {"Submarine","S" },
+            {"Carrier","A" },
+            {"Hit","X" },
+            {"Miss", "M" }
+        };
+        public string occupation { get; set; }
+
+        public Occupation(string s)
+        {
+            occupation = occupationType[s];
+        }
+            
+    }
+ 
+    public class Panel
+    {
+
+        public Cordinates Cordinates { get; set; }
+        public Occupation Occupation { get; set; }
+
+        public Panel(int row, int column, string status)
+        {
+            Cordinates = new Cordinates(row, column);
+            Occupation = new Occupation(status);
+
+        }
+
+        public string getOccupation()
+        {
+            return Occupation.ToString();
+        }
+    }
+}
--- BattleShip/Program.cs
+++ BattleShip/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,26 @@ namespace BattleShip
 {
     class Program
     {
+        private static void PrintProperties(Panel myObj)
+        {
+            foreach (var prop in myObj.GetType().GetProperties())
+            {
+                Console.WriteLine(prop.Name + ": " + prop.GetValue(myObj, null));
+            }
+
+            foreach (var field in myObj.GetType().GetFields())
+            {
+                Console.WriteLine(field.Name + ": " + field.GetValue(myObj));
+            }
+        }
         static void Main(string[] args)
         {
+            object panel = new Panel(1, 1, "Carrier");
+            //string status = panel.getOccupation();
+            Type type = panel.GetType();
+            foreach (PropertyInfo info in type.GetProperties()){
+                Console.WriteLine(info);
+            }
         }
     }
 }
