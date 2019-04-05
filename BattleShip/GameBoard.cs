cd7e66d97b5af35be187335f0dea73abe0c6e2e6
matt <matt.breck@live.com>
2019-04-05 16:58:09 -0400

    new

--- BattleShip/GameBoard.cs
+++ BattleShip/GameBoard.cs
@@ -8,6 +8,18 @@ namespace BattleShip
 {
     class GameBoard
     {
+        public List<Panel> Panels { get; set; }
+        public GameBoard()
+        {
+            Panels = new List<Panel>();
+            for (int i = 1; i <=10; i++)
+            {
+                for (int j = 1; j<=10; j++)
+                {
+                    Panels.Add(new Panel(i, j, "Empty"));
+                }
+            }
+        }
     }
     public class Cordinates
     {
@@ -61,4 +73,6 @@ namespace BattleShip
             return Occupation.ToString();
         }
     }
+
+   
 }
--- BattleShip/Program.cs
+++ BattleShip/Program.cs
@@ -9,26 +9,9 @@ namespace BattleShip
 {
     class Program
     {
-        private static void PrintProperties(Panel myObj)
-        {
-            foreach (var prop in myObj.GetType().GetProperties())
-            {
-                Console.WriteLine(prop.Name + ": " + prop.GetValue(myObj, null));
-            }
-
-            foreach (var field in myObj.GetType().GetFields())
-            {
-                Console.WriteLine(field.Name + ": " + field.GetValue(myObj));
-            }
-        }
         static void Main(string[] args)
         {
-            object panel = new Panel(1, 1, "Carrier");
-            //string status = panel.getOccupation();
-            Type type = panel.GetType();
-            foreach (PropertyInfo info in type.GetProperties()){
-                Console.WriteLine(info);
-            }
+            
         }
     }
 }
